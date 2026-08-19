9247d40ab208ab6c7bf9b4fbd10ed0573f840593
Armaan Alam <armaan.n.alam@gmail.com>
2026-08-19 15:58:40 +0530

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.cpp
@@ -0,0 +1,54 @@
+class Solution {
+
+    void BFS(vector<vector<char>>& grid, vector<vector<int>>& visit, int r, int c){
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<int,int>> q;
+
+        q.push({r, c});
+        visit[r][c] = 1;
+        
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, -1, 0, 1};
+
+        while(!q.empty()){
+
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for(int i = 0; i < 4; i++){
+                int row = r + dr[i];
+                int col = c + dc[i];
+
+                if(row >= 0 && col >= 0 && row < n && col < m && !visit[row][col] && grid[row][col] == '1'){
+                    visit[row][col] = 1;
+                    q.push({row, col});
+                }
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> visit(n, vector<int>(m, 0));
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(!visit[i][j] && grid[i][j] == '1'){
+                    cnt++;
+                    BFS(grid, visit, i, j);
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
