69fc73d028dc5b0ebeeaf98161860e2bb3230a68
Armaan Alam <armaan.n.alam@gmail.com>
2026-08-28 23:33:47 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp;
+        for(char ch : s){
+            if(isalnum(ch)) {
+                temp += tolower(ch);
+            }
+        }
+
+        int n = temp.size();
+
+        int left = 0;
+        int right = n-1;
+
+        while(left < right){
+            if(temp[left] != temp[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
