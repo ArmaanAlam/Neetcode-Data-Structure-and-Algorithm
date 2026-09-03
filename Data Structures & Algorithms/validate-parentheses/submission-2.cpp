ce99811b5a55ac0a4331a1cf98157b7659962a36
Armaan Alam <armaan.n.alam@gmail.com>
2026-09-03 18:29:08 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+        
+        for(char ch : s){
+            if(ch == '[' || ch == '{' || ch == '('){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+                char topchar = st.top();
+                if((ch == ')' && topchar != '(') ||
+                   (ch == ']' && topchar != '[') ||
+                   (ch == '}' && topchar != '{')){
+                        return false;
+                }
+                st.pop();
+            }
+        }
+
+        if(st.empty()) return true;
+
+        return false;
+    }
+};
